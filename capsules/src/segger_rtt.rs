944ea20d068b80bc40c40012f3fe9cf6d87ab38d
Brad Campbell <bradjc5@gmail.com>
2018-06-19 19:43:37 -0400

    segger rtt capsule cleaned up

--- capsules/src/segger_rtt.rs
+++ capsules/src/segger_rtt.rs
@@ -1,64 +1,102 @@
+//! Segger RTT implementation.
+//!
+//! RTT is a protocol for sending debugging messages to a connected host. The
+//! embedded platform configures a portion of memory in a special way, and then
+//! the host uses a JTAG connection to read the messages out of the chip's
+//! memory.
+//!
+//! Usage
+//! -----
+//!
+//! ```
+//! let virtual_alarm_rtt = static_init!(
+//!     capsules::virtual_alarm::VirtualMuxAlarm<'static, nrf5x::rtc::Rtc>,
+//!     capsules::virtual_alarm::VirtualMuxAlarm::new(mux_alarm)
+//! );
+//!
+//! let rtt_memory = static_init!(
+//!     capsules::segger_rtt::SeggerRttMemory,
+//!     capsules::segger_rtt::SeggerRttMemory::new(b"Terminal\0",
+//!         &mut capsules::segger_rtt::UP_BUFFER,
+//!         b"Terminal\0",
+//!         &mut capsules::segger_rtt::DOWN_BUFFER)
+//! );
+//!
+//! let rtt = static_init!(
+//!     capsules::segger_rtt::SeggerRtt<VirtualMuxAlarm<'static, nrf5x::rtc::Rtc>>,
+//!     capsules::segger_rtt::SeggerRtt::new(virtual_alarm_rtt, rtt_memory,
+//!         &mut capsules::segger_rtt::UP_BUFFER,
+//!         &mut capsules::segger_rtt::DOWN_BUFFER)
+//! );
+//! virtual_alarm_rtt.set_client(rtt);
+//! ```
 
-use kernel::common::cells::{TakeCell, OptionalCell};
+use kernel::common::cells::{OptionalCell, TakeCell};
 use kernel::hil;
 use kernel::hil::time::Frequency;
 
+/// Buffer for transmitting to the host.
 pub static mut UP_BUFFER: [u8; 1024] = [0; 1024];
-pub static mut DOWN_BUFFER: [u8; 32] = [0; 32];
-// pub static mut SEGGER_RTT_MEMORY: segger_rtt_memory =
 
+/// Buffer for receiving messages from the host.
+pub static mut DOWN_BUFFER: [u8; 32] = [0; 32];
+
+/// This structure is defined by the segger RTT protocol.
 #[repr(C)]
 pub struct SeggerRttMemory {
-	id: [u8; 16],
-	number_up_buffers: u32,
-	number_down_buffers: u32,
-	up_buffer: SeggerRttBuffer,
-	down_buffer: SeggerRttBuffer,
+    id: [u8; 16],
+    number_up_buffers: u32,
+    number_down_buffers: u32,
+    up_buffer: SeggerRttBuffer,
+    down_buffer: SeggerRttBuffer,
 }
 
 #[repr(C)]
 pub struct SeggerRttBuffer {
-	name: u32,
-	buffer: u32,
-	length: u32,
-	write_position: u32,
-	read_position: u32,
-	flags: u32,
+    name: *const (), // Pointer to the name of this channel. Must be a 4 byte thin pointer.
+    buffer: *const (), // Pointer to the buffer for this channel.
+    length: u32,
+    write_position: u32,
+    read_position: u32,
+    flags: u32,
 }
 
 impl SeggerRttMemory {
-	pub fn new(up_buffer_name: &'static [u8],
-		up_buffer: &'static [u8],
-		down_buffer_name: &'static [u8],
-		down_buffer: &'static [u8]) -> SeggerRttMemory {
-		SeggerRttMemory {
-			id: [0x53, 0x45, 0x47, 0x47, 0x45, 0x52, 0x20, 0x52, 0x54, 0x54, 0, 0, 0, 0, 0, 0],
-			number_up_buffers: 1,
-			number_down_buffers: 1,
-			up_buffer: SeggerRttBuffer {
-				name: up_buffer_name as *const [u8] as *const () as u32,
-				buffer: up_buffer as *const [u8] as *const () as u32,
-				length: 1024,
-				write_position: 0,
-				read_position: 0,
-				flags: 0
-			},
-			down_buffer: SeggerRttBuffer {
-				name: down_buffer_name as *const [u8] as *const () as u32,
-				buffer: down_buffer as *const [u8] as *const () as u32,
-				length: 32,
-				write_position: 0,
-				read_position: 0,
-				flags: 0
-			}
-		}
-	}
+    pub fn new(
+        up_buffer_name: &'static [u8],
+        up_buffer: &'static [u8],
+        down_buffer_name: &'static [u8],
+        down_buffer: &'static [u8],
+    ) -> SeggerRttMemory {
+        SeggerRttMemory {
+            // Must be "SEGGER RTT".
+            id: [
+                0x53, 0x45, 0x47, 0x47, 0x45, 0x52, 0x20, 0x52, 0x54, 0x54, 0, 0, 0, 0, 0, 0,
+            ],
+            number_up_buffers: 1,
+            number_down_buffers: 1,
+            up_buffer: SeggerRttBuffer {
+                name: up_buffer_name as *const [u8] as *const (),
+                buffer: up_buffer as *const [u8] as *const (),
+                length: 1024,
+                write_position: 0,
+                read_position: 0,
+                flags: 0,
+            },
+            down_buffer: SeggerRttBuffer {
+                name: down_buffer_name as *const [u8] as *const (),
+                buffer: down_buffer as *const [u8] as *const (),
+                length: 32,
+                write_position: 0,
+                read_position: 0,
+                flags: 0,
+            },
+        }
+    }
 }
 
-
 pub struct SeggerRtt<'a, A: hil::time::Alarm + 'a> {
-    // state: Cell<State>,
-    alarm: &'a A,
+    alarm: &'a A, // Dummy alarm so we can get a callback.
     config: TakeCell<'static, SeggerRttMemory>,
     up_buffer: TakeCell<'static, [u8]>,
     down_buffer: TakeCell<'static, [u8]>,
@@ -67,41 +105,21 @@ pub struct SeggerRtt<'a, A: hil::time::Alarm + 'a> {
 }
 
 impl<'a, A: hil::time::Alarm + 'a> SeggerRtt<'a, A> {
-	pub fn new(alarm: &'a A, config: &'static mut SeggerRttMemory, up_buffer: &'static mut [u8],
-        down_buffer: &'static mut [u8] ) -> SeggerRtt<'a, A> {
-		SeggerRtt {
-			alarm: alarm,
-			config: TakeCell::new(config),
-			up_buffer: TakeCell::new(up_buffer),
-			down_buffer: TakeCell::new(down_buffer),
-			client: OptionalCell::empty(),
-			client_buffer: TakeCell::empty(),
-		}
-	}
-
-	// pub fn say (&self) {
-	// 	self.up_buffer.map(|buffer| {
-	// 		self.config.map(|config| {
-	// 			// debug_gpio!(0, toggle);
-	// 			let mut index = config.up_buffer.write_position;
-
-	// 			buffer[index as usize] = 55;
-	// 			buffer[index as usize +1] = 10;
-
-	// 			index += 2;
-	// 			if index == config.up_buffer.length {
-	// 				index = 0;
-	// 			}
-	// 			config.up_buffer.write_position = index;
-	// 		});
-	// 	});
-
-
-
-	// 		// config.a = 0x33;
-	// 		// config.d = config.a  +config.b;
-	// 	// });
-	// }
+    pub fn new(
+        alarm: &'a A,
+        config: &'static mut SeggerRttMemory,
+        up_buffer: &'static mut [u8],
+        down_buffer: &'static mut [u8],
+    ) -> SeggerRtt<'a, A> {
+        SeggerRtt {
+            alarm: alarm,
+            config: TakeCell::new(config),
+            up_buffer: TakeCell::new(up_buffer),
+            down_buffer: TakeCell::new(down_buffer),
+            client: OptionalCell::empty(),
+            client_buffer: TakeCell::empty(),
+        }
+    }
 }
 
 impl<'a, A: hil::time::Alarm + 'a> hil::uart::UART for SeggerRtt<'a, A> {
@@ -109,65 +127,47 @@ impl<'a, A: hil::time::Alarm + 'a> hil::uart::UART for SeggerRtt<'a, A> {
         self.client.set(client);
     }
 
-    fn init(&self, params: hil::uart::UARTParams) {
-
-    }
+    fn init(&self, params: hil::uart::UARTParams) {}
 
     fn transmit(&self, tx_data: &'static mut [u8], tx_len: usize) {
-    	self.up_buffer.map(|buffer| {
-    		self.config.map(|config| {
-    			let mut index = config.up_buffer.write_position as usize;
-    			let buffer_len = config.up_buffer.length as usize;
+        self.up_buffer.map(|buffer| {
+            self.config.map(|config| {
+                // Copy the incoming data into the buffer. Once we increment
+                // the `write_position` the RTT listener will go ahead and read
+                // the message from us.
+                let mut index = config.up_buffer.write_position as usize;
+                let buffer_len = config.up_buffer.length as usize;
 
-    			for i in 0..tx_len {
-    				buffer[(i+index) % buffer_len] = tx_data[i];
-    			}
+                for i in 0..tx_len {
+                    buffer[(i + index) % buffer_len] = tx_data[i];
+                }
 
-    			// buffer[index as usize] = 55;
-    			// buffer[index as usize +1] = 10;
+                index = (index + tx_len) % buffer_len;
+                config.up_buffer.write_position = index as u32;
+            });
+        });
 
-    			index = (index + tx_len) % buffer_len;
-    			// if index >= config.up_buffer.length {
-    			// 	index = 0;
-    			// }
-    			config.up_buffer.write_position = index as u32;
-    		});
-    	});
+        // Save the client buffer so we can pass it back with the callback.
+        self.client_buffer.replace(tx_data);
 
-    	self.client_buffer.replace(tx_data);
-
-    	let interval = (100 as u32) * <A::Frequency>::frequency() / 1000000;
-    	let tics = self.alarm.now().wrapping_add(interval);
-    	self.alarm.set_alarm(tics);
-
-
-
+        // Start a short timer so that we get a callback and can issue the
+        // callback to the client.
+        let interval = (100 as u32) * <A::Frequency>::frequency() / 1000000;
+        let tics = self.alarm.now().wrapping_add(interval);
+        self.alarm.set_alarm(tics);
     }
 
-    fn receive(&self, rx_buf: &'static mut [u8], rx_len: usize) {
-    }
+    fn receive(&self, rx_buf: &'static mut [u8], rx_len: usize) {}
 
-    fn abort_receive(&self) {
-    }
+    fn abort_receive(&self) {}
 }
 
 impl<'a, A: hil::time::Alarm + 'a> hil::time::Client for SeggerRtt<'a, A> {
     fn fired(&self) {
-        // self.buffer.take().map(|buffer| {
-        //     // Turn on i2c to send commands.
-        //     self.i2c.enable();
-
-        //     buffer[0] = 0x02 as u8;
-        //     self.i2c.write_read(buffer, 1, 2);
-        //     self.state.set(State::ReadingLI);
-        // });
         self.client.map(|client| {
-	        self.client_buffer.take().map(|buffer| {
-	            client.transmit_complete(
-	                buffer,
-	                hil::uart::Error::CommandComplete,
-	            );
-	        });
+            self.client_buffer.take().map(|buffer| {
+                client.transmit_complete(buffer, hil::uart::Error::CommandComplete);
+            });
         });
     }
 }
